4b10f276109857ac78e8f0a70819691cb7d2f201
20170456 김찬영 <114966398+yeong978@users.noreply.github.com>
2022-12-23 22:56:04 +0900

    Update student20170456.py

--- HW4/student20170456.py
+++ HW4/student20170456.py
@@ -1,76 +1,67 @@
 import numpy as np
 import operator
+import os
 import sys
 
-trainingData = sys.argv[1]
-testData = sys.argv[2]
-
-def fileToMatrix(filename):
-    f = open(filename)
-    numberOfLines = len(f.readlines())
-    returnMat = np.zeros((numberOfLines, 32))
-    classLabelVector = []
-    f = open(filename)
-    index = 0
-    for line in f.readlines():
-        line = str.strip(line)
-        listFromLine = line.split('\t')
-        returnMat[index, :] = listFromLine[0:3]
-        classLabelVector.append(listFromLine[-1])
-        index += 1
-    return returnMat, classLabelVector
-
-datingDataMat, datingLabels = fileToMatrix('datingTestSet.txt')
-normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
-
-def createDataSet():
-    trainingFileList = listdr('trainingDigits')
-    m = len(trainingFileList)
-    tMat = zeros((m, 1024))
-    for i in range(m):
-        fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]
-        classNumStr = int(fileStr.split('_')[0])
-        hwLabels.append(classNumStr)
-        
-        classfierResult = classify0(testMat, tMat, labels, 20)
-        if classfierResult != classNumStr):
-            errorCount += 1
-        print(errorCount/mTest)
-        print()
-        
-
-def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
-    ranges = maxVals - minVals
-    normDataSet = np.zeros(np.shape(dataSet))
-    m = dataSet.shape[0]
-    normDataSet = dataSet - np.tile(minVals, (m, 1))
-    normDataSet = normDataSet / np.tile(ranges, (m, 1))
-    return normDataSet, ranges, minVals
-
-def classify0(inX, dataSet, labels, k):
+def classify(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
-    sqDistances = sqDiffMat.sum(axis = 1)
-    distances = sqDistances ** 0.5
-    sortedDistIndicies = distances.argsort()
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** .5
+    sortedDistIndices = distances.argsort()
     classCount = {}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
+        voteIlabel = labels[sortedDistIndices[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    sortedClassCount = sorted(classCount.items(),
-            key = operator.itemgetter(1), reverse = True)
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-    
+def file2matrix(fname):
+    returnVect = np.zeros((1, 1024))
+    with open(fname) as f:
+        for i in range(32):
+            line = f.readline()
+            for j in range(32):
+                returnVect[0, 32*i+j] = int(line[j])
+    return returnVect
 
-if __name__ == "__main__":
-    group, labels = createDataSet()
-    
-    for i in range(1,21):
-        result = classfy0(test, tMat, labels, i)
-        print()
-    
+def classifier(trainingDataPath, testDataPath):
+    trainingLabels = []
+    trainingFileList = os.listdir(trainingDataPath)
+    m = len(trainingFileList)
+    trainingMats = np.zeros((m, 1024))
+    for i in range(m):
+        fname = trainingFileList[i]
+        f = fname.split('.')[0]
+        classNum = int(f.split('_')[0])
+        trainingLabels.append(classNum)
+        trainingMats[i, :] = file2matrix(os.path.join(trainingDataPath, fname))
+
+    testLabels = []
+    testFileList = os.listdir(testDataPath)
+    m = len(testFileList)
+    testMats = np.zeros((m, 1024))
+    for i in range(m):
+        fname = testFileList[i]
+        f = fname.split('.')[0]
+        classNum = int(f.split('_')[0])
+        testLabels.append(classNum)
+        testMats[i, :] = file2matrix(os.path.join(testDataPath, fname))
+
+    for k in range(1, 21):
+        error = 0
+        resLabels = []
+        for testLabel, testMat in zip(testLabels, testMats):
+            res = classify(testMat, trainingMats, trainingLabels, k)
+            resLabels.append(res)
+            if res != testLabel:
+                error += 1
+        errRate = error / len(testMats) * 100
+        print(int(errRate))
+
+if __name__ == '__main__':
+    trainingDataPath = sys.argv[1]
+    testDataPath = sys.argv[2]
+
+    classifier(trainingDataPath, testDataPat
